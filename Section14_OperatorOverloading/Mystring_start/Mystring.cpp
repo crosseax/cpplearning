069ce38c8fb92843bd030712743244696d1a7fcd
crosseax <qq417737572@gmail.com>
2021-11-05 04:13:12 +0800

    started learning overloaded operators

---
+++ Section14_OperatorOverloading/Mystring_start/Mystring.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+#include <iostream>
+#include "Mystring.h"
+
+
+// no args constructor
+Mystring::Mystring ()
+    : str {nullptr} {
+        str = new char[1];
+        *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring (const char* s)
+    : str {nullptr} {
+        if (s == nullptr) {
+            str = new char[1];
+            *str = '\0';
+        } else {
+            str = new char[std::strlen(s) + 1];
+            std::strcpy(str, s);
+        }
+}
+
+// Copy constructor
+Mystring::Mystring (const Mystring &source)
+    : str {nullptr} {
+        str = new char[std::strlen(source.str) + 1];
+        std::strcpy(str, source.str);
+}
+
+// Destructor
+Mystring::~Mystring(){
+    delete [] str;
+}
+
+// Methods implementation
+void Mystring::display() const {
+    std::cout << "string: " << str << ", length: " << get_length() << std::endl;
+}
+
+int Mystring::get_length() const {
+    return std::strlen(str);
+}
+
+const char* Mystring::get_str() const {
+    return str;
+}
