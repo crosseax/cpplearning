d3aa19f9a6482ca4935dd3504e1e7c37c14fd37e
crosseax <qq417737572@gmail.com>
2021-11-12 14:45:52 +0800

    added notes

--- Section15_Inheritance/using_redefining_base_methods/Savings_Account.cpp
+++ Section15_Inheritance/using_redefining_base_methods/Savings_Account.cpp
@@ -6,17 +6,18 @@ std::ostream& operator<<(std::ostream& os, const Savings_Account& account) {
 }
 
 Savings_Account::Savings_Account(double balance, double intRate)
-    : Account {balance}, intRate {intRate} {
+    : Account {balance}, intRate {intRate} { // create constructor
 }
 
 Savings_Account::Savings_Account () // delegating this to the overloaded constructor
     : Savings_Account(0.0, 0.0) {
 }
 
-void Savings_Account::deposit(double amount) {
+void Savings_Account::deposit(double amount) { // override deposit methods
     amount = amount + (amount * intRate/100);
     // balance += amount;
     // std::cout << "Savings Account deposit called with " << amount << std::endl; 
     Account::deposit(amount); // delegating to the parent method
 }
 
+// the withdraw method is inherited
