0626bf999070eebc61d7770417ff81a76ec20db5
crosseax <qq417737572@gmail.com>
2021-11-15 00:31:45 +0800

    a better way to do timer using obj lifetime in cpp

--- Extra_Material/timer_chrono_usage.cpp
+++ Extra_Material/timer_chrono_usage.cpp
@@ -1,25 +1,58 @@
 // This files shows a use case for timing program in .cpp programming
 // Including a brief look to <chrono> and <thread> library
 
-// this compiles after -std=c++14
 
 #include <iostream>
 #include <chrono>
 #include <thread>
 
+// This compiles after -std=c++11;
+class Timer { // so now use obj life time to automatically time things instead of writing lots of codes below
+public:
+        std::chrono::time_point<std::chrono::steady_clock> start, end;
+        std::chrono::duration<float> duration;
 
+    Timer() {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer() {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+
+        float ms = duration.count() * 1000.0f; // by default, duration.count() returns in seconds
+        std::cout << "Timer took " << ms << " ms " << std::endl;
+    }
+};
+// by using the obj lifetime above, 
+// just create Timer obj at the beginning of the code you wanna time
+// then destruct it when you want to stop the Timer
+
+void say_hello() {
+    for (int i = 0; i < 1000; i++) {
+        std::cout << "Hello" << std::endl;
+    }
+}
 
 int main (void)
 {
-    using namespace std::literals::chrono_literals; // enable using 1s as 1 second in the later code
+    Timer timer;
+    say_hello();
+    std::this_thread::sleep_for(std::chrono::seconds(1)); // program sleep for one second
 
-    auto start = std::chrono::high_resolution_clock::now(); // get the time for now
-    std::this_thread::sleep_for(1s); // pause the program for 1 second
-    auto end = std::chrono::high_resolution_clock::now(); // get the time for now
 
-    std::chrono::duration<float> duration; // get the during in format of floats (so more precise) 
-    duration = end - start; 
-    std::cout << duration.count() << " seconds " << std::endl;
+
+
+    // codes below are the abundant way of doing timing in cpp
+    // this compiles after -std=c++14
+
+    // auto start = std::chrono::high_resolution_clock::now(); // get the time for now
+    // std::this_thread::sleep_for(std::chrono::seconds(1)); // pause the program for 1 second
+    // auto end = std::chrono::high_resolution_clock::now(); // get the time for now
+
+    // std::chrono::duration<float> duration; // get the during in format of floats (so more precise) 
+    // duration = end - start; 
+    // std::cout << duration.count() << " seconds " << std::endl;
 
 
     return 0;
