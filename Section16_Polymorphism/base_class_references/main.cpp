f079dc5a3c308f6ec5d55b5089f3175a3d2eb5d9
crosseax <qq417737572@gmail.com>
2021-11-16 11:00:34 +0800

    base class reference. Qeustion: default copy do not copy virtual destructor?

---
+++ Section16_Polymorphism/base_class_references/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+class Account {
+public:
+    virtual void withdraw(double amount){ 
+        std::cout << "Account::withdraw(amount), $" << amount << std::endl;
+    }
+
+    virtual ~Account() {
+        std::cout << "Account::destructor" << std::endl;
+    }
+};
+
+class Checking: public Account {
+public:
+    virtual void withdraw(double amount){
+        std::cout << "Checking::withdraw(amount), $" << amount << std::endl;
+    }
+    virtual ~Checking() {
+        std::cout << "Checking::destructor, ";
+    }
+};
+
+class Savings: public Account {
+public:
+    virtual void withdraw(double amount){
+        std::cout << "Savings::withdraw(amount), $" << amount << std::endl;
+    }
+    virtual ~Savings() {
+        std::cout << "Savings::destructor, ";
+    }
+};
+
+class Trust: public Savings {
+public:
+    virtual void withdraw(double amount){
+        std::cout << "Trust::withdraw(amount), $" << amount << std::endl;
+    }
+    virtual ~Trust() {
+        std::cout << "Trust::destructor, ";
+    }
+};
+
+
+// Use Base class reference here
+// to think generally and abstratly
+
+void do_withdraw(Account &account, double amount) {
+    account.withdraw(amount);
+}
+
+int main (void)
+{
+    Account a;
+    Account &ref1 = a;
+    ref1.withdraw(1000);
+
+    Trust t;
+    Account &ref2 = t;
+    ref2.withdraw(1000); 
+
+    
+    // Use Base class reference
+    Account a1;
+    Savings a2;
+    Checking a3;
+    Trust a4;
+
+    do_withdraw(a1, 1000);
+    do_withdraw(a2, 2000);
+    do_withdraw(a3, 3000);
+    do_withdraw(a4, 4000);
+
+    std::cout << "\n===No pointers vector===" << std::endl;
+    {
+        std::vector<Account> accounts;
+        accounts.push_back(a1);
+        accounts.push_back(a2);
+        accounts.push_back(a3);
+        accounts.push_back(a4);
+
+        for (auto acc : accounts) {
+            do_withdraw(acc, 500);
+        }
+        // notice this will not call virtual destructors
+    }
+
+    // However, the method above is making a lot of copy
+    // so the destructor will be called each time a function is used
+    
+
+    std::cout << "\n===Pointers vector===" << std::endl;
+    // Method below use pointer to not to create copy, hence boost performance
+    {
+        std::vector<Account*> accountsPtr;
+        accountsPtr.push_back(&a1);
+        accountsPtr.push_back(&a2);
+        accountsPtr.push_back(&a3);
+        accountsPtr.push_back(&a4);
+
+        for (auto acc : accountsPtr) {
+            do_withdraw(*acc, 500);
+        }
+    }
+
+    // Destructor calls
+    std::cout << "\n===Destructors===" << std::endl;
+
+
+    return 0;
+}
