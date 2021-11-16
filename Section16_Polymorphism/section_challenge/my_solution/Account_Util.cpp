367fd9fd30cc9b709f8f6191ad19a603c66545fd
crosseax <qq417737572@gmail.com>
2021-11-16 18:20:06 +0800

    simplified Account Utility functions

--- Section16_Polymorphism/section_challenge/my_solution/Account_Util.cpp
+++ Section16_Polymorphism/section_challenge/my_solution/Account_Util.cpp
@@ -2,131 +2,34 @@
 #include "Account_Util.h"
 
 
-// Utility helper functions for Account class
+// Utility helper functions for Account* class
 
-void display(const std::vector<Account>& accounts) {
+void display(const std::vector<Account*>& accounts) {
     std::cout << "\n===Accounts===" << std::endl;
-    for (const auto& acc : accounts) {
-        std::cout << acc << std::endl;
+    for (const auto acc : accounts) {
+        std::cout << *acc << std::endl;
     }
 }
 
-void deposit(std::vector<Account>& accounts, double amount) {
+void deposit(std::vector<Account*>& accounts, double amount) {
     std::cout << "\n===Depositing to Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.deposit(amount)){
-            std::cout << "Deposited $" << amount << " to " << acc << std::endl;
+    for (auto acc : accounts) {
+        if (acc->deposit(amount)){
+            std::cout << "Deposited $" << amount << " to " << *acc << std::endl;
         } else {
-            std::cout << "Failed deposit of $" << amount << " to " << acc << std::endl;
+            std::cout << "Failed deposit of $" << amount << " to " << *acc << std::endl;
         }
     }
 }
 
-void withdraw(std::vector<Account>& accounts, double amount) {
+void withdraw(std::vector<Account*>& accounts, double amount) {
     std::cout << "\n===Withdrawing from Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.withdraw(amount)){
-            std::cout << "Withdrew $" << amount << " from " << acc << std::endl;
+    for (auto acc : accounts) {
+        if (acc->withdraw(amount)){
+            std::cout << "Withdrew $" << amount << " from " << *acc << std::endl;
         } else {
-            std::cout << "Failed withdrawal of $" << amount << " from " << acc << std::endl;
+            std::cout << "Failed withdrawal of $" << amount << " from " << *acc << std::endl;
         }
     }
 }
 
-
-// Utility helper functions for Savings_Account class
-
-void display(const std::vector<Savings_Account>& accounts) {
-    std::cout << "\n===Savings Accounts===" << std::endl;
-    for (const auto& acc : accounts) {
-        std::cout << acc << std::endl;
-    }
-}
-
-void deposit(std::vector<Savings_Account>& accounts, double amount) {
-    std::cout << "\n===Depositing to Savings Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.deposit(amount)){
-            std::cout << "Deposited $" << amount << " to " << acc << std::endl;
-        } else {
-            std::cout << "Failed deposit of $" << amount << " to " << acc << std::endl;
-        }
-    }
-}
-
-void withdraw(std::vector<Savings_Account>& accounts, double amount) {
-    std::cout << "\n===Withdrawing from Savings Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.withdraw(amount)){
-            std::cout << "Withdrew $" << amount << " from " << acc << std::endl;
-        } else {
-            std::cout << "Failed withdrawal of $" << amount << " from " << acc << std::endl;
-        }
-    }
-}
-
-
-
-// Utility helper functions for Checking_Account class
-
-
-void display(const std::vector<Checking_Account>& accounts) {
-    std::cout << "\n===Checking_Accounts===" << std::endl;
-    for (const auto& acc : accounts) {
-        std::cout << acc << std::endl;
-    }
-}
-
-void deposit(std::vector<Checking_Account>& accounts, double amount) {
-    std::cout << "\n===Depositing to Checking Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.deposit(amount)){
-            std::cout << "Deposited $" << amount << " to " << acc << std::endl;
-        } else {
-            std::cout << "Failed deposit of $" << amount << " to " << acc << std::endl;
-        }
-    }
-}
-
-
-void withdraw(std::vector<Checking_Account>& accounts, double amount) {
-    std::cout << "\n===Withdrawing from Checking Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.withdraw(amount)){
-            std::cout << "Withdrew $" << amount << " from " << acc << std::endl;
-        } else {
-            std::cout << "Failed withdrawal of $" << amount << " from " << acc << std::endl;
-        }
-    }
-}
-
-// Utility helper functions for Checking_Account class
-
-void display(const std::vector<Trust_Account>& accounts) {
-    std::cout << "\n===Trust Accounts===" << std::endl;
-    for (const auto& acc : accounts) {
-        std::cout << acc << std::endl;
-    }
-}
-
-void deposit(std::vector<Trust_Account>& accounts, double amount) {
-    std::cout << "\n===Depositing to Trust Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.deposit(amount)){
-            std::cout << "Deposited $" << amount << " to " << acc << std::endl;
-        } else {
-            std::cout << "Failed deposit of $" << amount << " to " << acc << std::endl;
-        }
-    }
-}
-
-void withdraw(std::vector<Trust_Account>& accounts, double amount) {
-    std::cout << "\n===Withdrawing from Trust Accounts===" << std::endl;
-    for (auto& acc : accounts) {
-        if (acc.withdraw(amount)){
-            std::cout << "Withdrew $" << amount << " from " << acc << std::endl;
-        } else {
-            std::cout << "Failed withdrawal of $" << amount << " from " << acc << std::endl;
-        }
-    }
-}
--- Section16_Polymorphism/section_challenge/my_solution/main.cpp
+++ Section16_Polymorphism/section_challenge/my_solution/main.cpp
@@ -10,22 +10,34 @@ using namespace std;
 
 int main (void) 
 {
-    Checking_Account c;
-    cout << c << endl;
+    Checking_Account frank {"Frank", 5000};
 
-    Savings_Account s {"Frank", 5000, 2.6};
-    cout << s << endl;
-    s.deposit(10000);
-    cout << s << endl;
-    s.withdraw(10000);
-    cout << s << endl;
-
-
-    Account* ptr = new Trust_Account("Leo", 10000, 3.4);
-    cout << *ptr << endl;
+    Account* trust = new Trust_Account("Leo", 10000, 3.4);
+    cout << *trust << endl;
     
+    Account* p1 = new Checking_Account("Larry", 10000);
+    Account* p2 = new Savings_Account("Moe", 10000);
+    Account* p3 = new Trust_Account("Curly");
+    Account* p4 = new Trust_Account("Garry", 100000);
+
+    std::vector<Account*> accounts {p1, p2, p3, p4};
+
+    display(accounts);
+    deposit(accounts, 1000);
+    withdraw(accounts, 2000);
+    withdraw(accounts, 2000);
+    withdraw(accounts, 2000);
+    withdraw(accounts, 2000);
+
+    display(accounts);
 
     cout << endl;
+
+    delete p1;
+    delete p2; 
+    delete p3;
+
+    
 
     return 0;
 }
