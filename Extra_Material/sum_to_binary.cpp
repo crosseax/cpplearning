c949d287f42d287092d9cb544c7a56487410fdd4
crosseax <qq417737572@gmail.com>
2021-11-19 14:23:44 +0800

    did a problem on codewars, probably the worst implementation you'll ever see

---
+++ Extra_Material/sum_to_binary.cpp
@@ -0,0 +1,46 @@
+// This is a question from codewars.com
+
+#include <iostream>
+#include <string>
+
+
+std::string add_binary(uint64_t a, uint64_t b) {
+    std::string tmp;
+    uint64_t sum = a + b;
+    // sum *= 2;
+    // std::cout << sum << std::endl;
+    if (sum > 0) {
+        while (sum > 0) {
+        int bi = sum % 2;
+        if (bi == 1) {
+            tmp = tmp + '1';
+        } else {
+            tmp = tmp + '0';
+        }
+        // std::cout << tmp << " ";
+        sum /= 2;
+        }
+    // std::cout << tmp << std::endl;
+    } else {
+        tmp = '0';
+    }
+    std::string result;
+    for (size_t i = 0; i < tmp.length(); i++) {
+        result += tmp[tmp.length() - 1 - i];
+    }
+    std::cout << result << std::endl;
+    return result;
+}
+
+
+int main (void) 
+{
+    
+    add_binary(5,3);
+    add_binary(0,0);
+    add_binary(1,11);
+    add_binary(1109220521037,921633258894011);
+    
+
+    return 0;
+}
