77bee25db1ce52cef6e8ef857b710c6e7af1b86e
crosseax <qq417737572@gmail.com>
2021-11-19 14:29:24 +0800

    a way-better implementation

--- Extra_Material/sum_to_binary.cpp
+++ Extra_Material/sum_to_binary.cpp
@@ -4,32 +4,44 @@
 #include <string>
 
 
+// std::string add_binary(uint64_t a, uint64_t b) {
+//     std::string tmp;
+//     uint64_t sum = a + b;
+//     // sum *= 2;
+//     // std::cout << sum << std::endl;
+//     if (sum > 0) {
+//         while (sum > 0) {
+//         int bi = sum % 2;
+//         if (bi == 1) {
+//             tmp = tmp + '1';
+//         } else {
+//             tmp = tmp + '0';
+//         }
+//         // std::cout << tmp << " ";
+//         sum /= 2;
+//         }
+//     // std::cout << tmp << std::endl;
+//     } else {
+//         tmp = '0';
+//     }
+//     std::string result;
+//     for (size_t i = 0; i < tmp.length(); i++) {
+//         result += tmp[tmp.length() - 1 - i];
+//     }
+//     std::cout << result << std::endl;
+//     return result;
+// }
+
 std::string add_binary(uint64_t a, uint64_t b) {
-    std::string tmp;
-    uint64_t sum = a + b;
-    // sum *= 2;
-    // std::cout << sum << std::endl;
-    if (sum > 0) {
-        while (sum > 0) {
-        int bi = sum % 2;
-        if (bi == 1) {
-            tmp = tmp + '1';
-        } else {
-            tmp = tmp + '0';
-        }
-        // std::cout << tmp << " ";
-        sum /= 2;
-        }
-    // std::cout << tmp << std::endl;
-    } else {
-        tmp = '0';
-    }
-    std::string result;
-    for (size_t i = 0; i < tmp.length(); i++) {
-        result += tmp[tmp.length() - 1 - i];
-    }
-    std::cout << result << std::endl;
-    return result;
+    a += b;
+    std::string output;
+
+    do {
+        output = std::to_string(a % 2) + output;
+        a /= 2;
+    } while(a > 0);
+    std::cout << output << std::endl;   
+    return output;
 }
 
 
