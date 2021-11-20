088c518338df75d307b7f017481fefd442c30007
crosseax <qq417737572@gmail.com>
2021-11-21 03:18:12 +0800

    setting up for challenge 1, I'm doing dos Excel just like in 1960s

---
+++ Section19_IOandStreams/section_challenge_1/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+// struct is like a class by everything is public
+// most of time in cpp it's just used to collect data
+struct City { 
+    std::string name;
+    long population;
+    double cost;
+};
+
+struct Country {
+    std::string name;
+    std::vector<City> cities;
+};
+
+struct Tours {
+    std::string title;
+    std::vector<Country> countries;
+};
+
+int main (void)
+{
+    Tours tours
+        {
+            "Tours Ticket Prices from Miami",
+            {
+                {
+                    "Colombia",
+                    {
+                        {"Bogota", 8778000, 400.98},
+                        {"Cali", 2401000, 424.12},
+                        {"Medellin", 2464000, 350.98},
+                        {"Cartagena", 972000, 345.98}
+                    },
+                },
+                {
+                    "Brazil",
+                    {
+                        {"Rio De Janiero", 13500000, 567.45},
+                        {"Sao Paulo", 11310000, 975.45},
+                        {"Salvador", 18234000, 855.99}
+                    },
+                },
+                {
+                    "Chile",
+                    {
+                        {"Valdivia", 260000, 569.12},
+                        {"Santiago", 7040000, 520.00}
+                    },
+                },
+                {
+                    "Argentina",
+                    {
+                        {"Buenos Aires", 3010000, 723.77}
+                    }
+                }
+            }
+        };
+
+    // Unformatted
+    std::cout << tours.title << std::endl;
+    for (auto country : tours.countries) {
+        std::cout << country.name << std::endl;
+        for (auto city : country.cities) {
+            std::cout << "\t" << city.name
+                      << "\t" << city.population
+                      << "\t" << city.cost
+                      << std::endl;
+        }
+    }
+
+    std::cout << std::endl; 
+
+    return 0;
+}
