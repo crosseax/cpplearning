8925f2669da5c25ed1ddd4a5f892c0b19a88d3cc
crosseax <qq417737572@gmail.com>
2021-11-28 00:48:18 +0800

    basic notes about the queue

---
+++ Section20_STL/adaptors/queue/basic_notes.cpp
@@ -0,0 +1,29 @@
+// std::queue
+
+// First-in First-out (FIFO) data structure
+// Implemented as an adpter over other STL container.
+// Can be implemented as a list, or deque
+// Elements are pushed at the back and popped from the front
+// No iterators are supported
+
+
+// #include <queue>
+
+// push - insert an element at the back of the queue
+// pop - remove an element from the front of the queue
+
+// back - access the element at the back
+// front - access the element at the front
+
+// empty - is the queue empty or not
+// size - number of elements in the queue
+
+
+
+// std::queue initialization
+
+    // std::queue<int> s;                       // deque
+    // std::queue<int, std::list<int>> s1;      // list
+    // std::queue<int, std::deque<int>> s1;     // deque
+
+
