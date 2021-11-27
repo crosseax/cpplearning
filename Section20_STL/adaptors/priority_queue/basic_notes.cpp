e4af746b34ff9d56f6ea4cfc04995cfa0e05acb2
crosseax <qq417737572@gmail.com>
2021-11-28 01:43:34 +0800

    why stack top, queue front and back, and priority_queue top again, like who did this

---
+++ Section20_STL/adaptors/priority_queue/basic_notes.cpp
@@ -0,0 +1,28 @@
+// std::priority_queue
+
+// Allows insertions and removal of elements in order from the front of the container
+// Elements are stored internally as a vector by default
+    // a data structure called heap is used behind the scene (NOT THE HEAP OF MEMORY)
+// Elements are inserted in priority order
+    // largest value will always be at the front
+// No iterators are supported
+
+
+
+// #include <queue>
+
+// push - insert an element into sorted order
+// pop - remove the top element (greatest)
+
+// top - access the top element (greatest)
+
+// empty - is the queue empty or not
+// size - number of elements in the queue
+
+
+
+// std::priority_queue initialization
+
+    // std::priority_queue<int> pq;                      // vector
+
+
