da4bf93568f3efe5c804677f2b767845080e14fe
crosseax <qq417737572@gmail.com>
2021-11-28 19:54:59 +0800

    motivations for the creation of lambda expressions

---
+++ Section21_LambdaExpressions/motivation/basic_notes.cpp
@@ -0,0 +1,22 @@
+// Motivation
+
+// Prior to C++11
+    // function objects
+    // function pointers
+
+    // often write many short functions that control algorithms
+    
+    // these short functions would be encapsulated in small classes to produce function objects
+    
+    // many times the classes and functions are far removed from where they are used 
+    // leading to modification, maintenance, and testing issues
+
+    // compiler cannot effectively inline these functions for efficiency
+
+
+
+
+
+
+
+    
